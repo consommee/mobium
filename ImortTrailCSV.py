9cf533ee1077228709fcb97bac37e011e9cad9cc
Yoshimasa Matsumoto <39672851+YoshiMatsumoto@users.noreply.github.com>
2019-04-07 17:15:02 +0900

    Update ImortTrailCSV.py

--- ImortTrailCSV.py
+++ ImortTrailCSV.py
@@ -9,20 +9,23 @@ def main():
     lns = []
     #prompt the user for a file to import
     filter = "csv file (*.csv)|*.csv|All Files (*.*)|*.*||"
-    filename = rs.OpenFileName("Open Point File", filter)
+    filename = rs.OpenFileName("Open csv File", filter)
     if not filename: return
 
     with open(filename, "r") as f:
         reader = csv.reader(f)
+        
         for i in range(10):
             tmp = []
+            j = 0
             for row in reader:
+                j += 1
+                print(j)
                 if float(row[3]) == i:
-                    tmp.append((float(row[1]),float(row[2]), 0))
-            t_tmp = tuple(tmp)
-            rs.AddPolyline(t_tmp)
-            del tmp[:]
-
+                    tmp.append((float(row[1])+j * 1,float(row[2]), 0))
+            ln = rs.AddPolyline(tmp)
+            lns.append(ln)
+    return lns
 
 if __name__ == '__main__':
     main()
